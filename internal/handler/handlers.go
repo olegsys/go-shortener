92abc33b26736a2fb70eb49a10bbf88776e990d9
Oleg Sakhabutdinov <Oleg.Sakhabutdinov@gs-labs.ru>
2026-03-02 21:28:24 +0500

    fix id get

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 
-	"github.com/go-chi/chi/v5"
 	"github.com/olegsys/go-shortener/internal/model"
 )
 
@@ -36,7 +36,7 @@ func (h *Handler) Shorten(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) Redirect(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
+	id := strings.TrimPrefix(r.URL.Path, "/")
 	if id == "" {
 		fmt.Println("Empty ID")
 		http.Error(w, "ID is required", http.StatusBadRequest)
