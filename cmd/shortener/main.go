313db499fea69ebd43a163b79c38582d5bb541cb
Oleg Sakhabutdinov <Oleg.Sakhabutdinov@gs-labs.ru>
2026-03-02 17:16:17 +0500

    alpha version

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/olegsys/go-shortener/internal/handler"
+	"github.com/olegsys/go-shortener/internal/model"
+)
+
+func main() {
+	store := model.NewStore()
+	h := &handler.Handler{
+		Store: store,
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			h.Shorten(w, r)
+		case http.MethodGet:
+			h.Redirect(w, r)
+		default:
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	})
+	err := http.ListenAndServe(`:8080`, nil)
+	if err != nil {
+		panic(err)
+	}
+}
