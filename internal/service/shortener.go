b9c10de3fc4c3e8c553ceeb68f1c937b88aace9e
Oleg Sakhabutdinov <Oleg.Sakhabutdinov@gs-labs.ru>
2026-03-03 21:44:35 +0500

    try to fix /

--- internal/service/shortener.go
+++ internal/service/shortener.go
@@ -25,7 +25,7 @@ func NewShortenerService(storage URLStore, baseUrl string) *ShortenerService {
 func (s *ShortenerService) Shorten(longUrl string) string {
 	shortURL := GenerateID()
 	s.storage.Set(shortURL, longUrl)
-	return s.baseURL + shortURL
+	return s.baseURL + "/" + shortURL
 }
 
 func (s *ShortenerService) Resolve(shortURL string) (string, bool) {
