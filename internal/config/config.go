5425e1d96ad70398ed17b37c40808154216f68c8
Oleg Sakhabutdinov <Oleg.Sakhabutdinov@gs-labs.ru>
2026-03-03 21:57:41 +0500

    refactor config for linter

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,13 +4,13 @@ import "flag"
 
 type Config struct {
 	ListenAddress string
-	BaseUrl       string
+	BaseURL       string
 }
 
 func LoadConfig() *Config {
 	cfg := &Config{}
 	flag.StringVar(&cfg.ListenAddress, "a", ":8080", "address")
-	flag.StringVar(&cfg.BaseUrl, "b", "http://localhost:8080/", "base url")
+	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080/", "base url")
 	flag.Parse()
 	return cfg
 }
