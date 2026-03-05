2a94ee73773afab89d297231844cd02bc3e58ea4
Oleg Sakhabutdinov <Oleg.Sakhabutdinov@gs-labs.ru>
2026-03-05 14:31:57 +0500

    add redirect tests

--- internal/handler/handlers_test.go
+++ internal/handler/handlers_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestShorten(t *testing.T) {
+func TestHandler_Shorten(t *testing.T) {
 	storage := repository.NewMapStorage()
 	svc := service.NewShortenerService(storage, "http://localhost:8080/")
 	h := NewHandler(svc)
@@ -87,6 +87,57 @@ func TestShorten(t *testing.T) {
 	}
 }
 
-func TestRedirect(t *testing.T) {
+func TestHandler_Redirect(t *testing.T) {
+	mockData := struct {
+		baseURL string
+		id      string
+		longURL string
+	}{"http://localhost:8080/", "abc", "https://yandex.ru/test"}
 
+	storage := repository.NewMapStorage()
+	storage.Set(mockData.id, mockData.longURL)
+	svc := service.NewShortenerService(storage, mockData.baseURL)
+	h := NewHandler(svc)
+
+	router := chi.NewRouter()
+	router.Get("/{id}", h.Redirect)
+
+	tests := []struct {
+		name         string
+		method       string
+		id           string
+		wantStatus   int
+		wantLocation string
+	}{
+		{
+			name:         "success redirect",
+			method:       http.MethodGet,
+			id:           mockData.id,
+			wantStatus:   http.StatusTemporaryRedirect,
+			wantLocation: mockData.longURL,
+		},
+		{
+			name:       "url not found",
+			id:         "fake",
+			method:     http.MethodGet,
+			wantStatus: http.StatusBadRequest,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := httptest.NewRequest(tt.method, mockData.baseURL+tt.id, nil)
+			w := httptest.NewRecorder()
+
+			router.ServeHTTP(w, r)
+			res := w.Result()
+			defer res.Body.Close()
+
+			assert.Equal(t, tt.wantStatus, res.StatusCode)
+			if tt.wantLocation != "" {
+				assert.Equal(t, tt.wantLocation, res.Header.Get("Location"))
+			}
+
+		})
+	}
 }
